2065730f3ad2ba00f5d6f9a8e6a4dc06185f43e2
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-01 13:31:38 +0900

    nvim-tree: Change key map, after this tree is displayed collectly

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -3,7 +3,7 @@ local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- nvim tree
-map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+vim.api.nvim_set_keymap('n', '<C-e>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 -- telescope
 map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
