b30f0ec2de5125682161893f597f2ee37e63be75
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-01 13:24:11 +0900

    lualine: Change color scheme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
   "nvim-lualine/lualine.nvim",
   config = function()
     require("lualine").setup({
-      options = { theme = "gruvbox" }
+      options = { theme = "onedark" }
     })
   end
 }
