85414c399e30b19ae261bdce47d23f03426e7b25
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-01 13:31:38 +0900

    nvim-tree: Change key map, after this, tree is displayed collectly

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -3,7 +3,7 @@ local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- nvim tree
-map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+map("n", "<C-e>", ":NvimTreeToggle<CR>", opts)
 
 -- telescope
 map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -9,3 +9,4 @@ vim.opt.mouse="" -- disable mouse
 vim.g.python3_host_prog = '~/.pyenv/versions/3.12.11/bin/python' -- specify python under pyenv
 vim.g.ruby_host_prog = '~/.rbenv/versions/3.4.7/bin/ruby' -- specify ruby under rbenv
 vim.g.loaded_perl_provider = 0 -- disable perl provider
+--vim.g.mapleader = " " -- <leader>キーをバックスラッシュからスペースに変更する
