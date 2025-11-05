c0b11d8e345ae563e140d1c6ce0fa37d492a33db
Yoshimasa Obayashi <obayashi.yoshimasa@socionext.com>
2025-11-05 21:17:46 +0900

    none-ls: Debugged
      none-ls builtin formatters and diagnostics are listed in github
      it does not include eslint_d, shellcheck, flake8 in diagnostics
      so this commit selects builtin ones

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,26 +1,24 @@
 -- lua/plugins/none-ls.lua
 return {
   "nvimtools/none-ls.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
 
     null_ls.setup({
       sources = {
-        -- フォーマッタ
+        -- Formatting
         null_ls.builtins.formatting.stylua.with({ filetypes = { "lua" } }),
+        null_ls.builtins.formatting.shfmt.with({ filetypes = { "sh" } }),
         null_ls.builtins.formatting.prettierd.with({ filetypes = { "javascript", "typescript", "json", "markdown" } }),
         null_ls.builtins.formatting.black.with({ filetypes = { "python" } }),
 
-        -- Linter
-        null_ls.builtins.diagnostics.eslint_d.with({ filetypes = { "javascript", "typescript" } }),
-        null_ls.builtins.diagnostics.shellcheck.with({ filetypes = { "sh", "bash", "zsh" } }),
-        null_ls.builtins.diagnostics.flake8.with({
-          filetypes = { "python" },
-          condition = function(utils)
-            return utils.root_has_file({ ".flake8", "setup.cfg", "pyproject.toml" })
-          end,
-        }),
+        -- Diagnostics(Linter)
+        null_ls.builtins.diagnostics.selene.with({ filetypes = { "lua", "luau" } }),
+        null_ls.builtins.diagnostics.dotenv_linter.with({ filetypes = { "sh" } }),
+        null_ls.builtins.diagnostics.vale.with({ filetypes = { "markdown", "tex", "asciidoc" } }),
       },
     })
   end,
