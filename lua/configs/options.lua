f7abb96ccbd734eb815d6995c7a1cb569051b5b8
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-07 23:21:05 +0900

    options: Add comments

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -1,12 +1,16 @@
 -- lua/config/options.lua
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.termguicolors = true
+-- disable mouse
 vim.opt.mouse="" -- disable mouse
-vim.g.python3_host_prog = '~/.pyenv/versions/3.12.11/bin/python' -- specify python under pyenv
-vim.g.ruby_host_prog = '~/.rbenv/versions/3.4.7/bin/ruby' -- specify ruby under rbenv
-vim.g.loaded_perl_provider = 0 -- disable perl provider
+
+-- specify python under pyenv
+vim.g.python3_host_prog = '~/.pyenv/versions/3.12.11/bin/python'
+-- specify ruby under rbenv
+vim.g.ruby_host_prog = '~/.rbenv/versions/3.4.7/bin/ruby'
+-- disable perl provider
+vim.g.loaded_perl_provider = 0
+
+--vim.opt.tabstop = 4
+--vim.opt.shiftwidth = 4
+--vim.opt.expandtab = true
+
 --vim.g.mapleader = " " -- <leader>キーをバックスラッシュからスペースに変更する
