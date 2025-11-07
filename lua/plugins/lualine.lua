dd4344daaf7426e9ba106347c227fac4130e5cec
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-08 01:06:15 +0900

    lualine: Add dependencies

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,14 @@
 -- lua/plugins/lualine.lua
+-- status line
 return {
   "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("lualine").setup({
-      options = { theme = "onedark" }
+      options = {
+        theme = "onedark",
+        globalstatus = true,
+      },
     })
-  end
+  end,
 }
