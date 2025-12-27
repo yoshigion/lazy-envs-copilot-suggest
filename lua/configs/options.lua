9670ae9353bd79f020260f4b1eb5b3009c9cd534
Yoshimasa Obayashi <obayashi.yoshimasa@socionext.com>
2025-12-27 10:12:52 +0900

    configs/options.lua: Add clipboard settings between wsl2 and windows

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -18,3 +18,22 @@ vim.opt.termguicolors = true
 
 ---- <leader>キーをバックスラッシュからスペースに変更する
 vim.g.mapleader = " "
+
+---- clipboard sharing between wsl2 and windows
+vim.opt.clipboard = "unnamedplus"
+--vim.opt.clipboard = "unnamed"
+
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
